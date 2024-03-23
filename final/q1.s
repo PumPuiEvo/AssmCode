02ee36cc49635cc13173f33572e277e0f232789d
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-03-23 18:45:52 +0700

    q1 finish

--- final/q1.s
+++ final/q1.s
@@ -9,67 +9,58 @@ section .data
     NOTINPUT equ -1
 
     testNumber dq 0xABCDEF0011223344
-    hexuse dw 0
     numTwo db 2
 
     axeIs db "AX = "
-    temp db ""
-    biTem2 db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-
-section .bss
-    biTem resb 16
+    endline db "",10
+    biTem3 db 0
 
 section .text
 _start:
-    mov rax, qword[testNumber]
-    mov word[hexuse], ax
-    movzx rax, ax
-
     mov rax, SYS_write
     mov rdi, STDOUT
     mov rsi, axeIs
     mov rdx, 5
     syscall
+
+    mov rax, qword[testNumber]
+    movzx rax, ax
 
     mov r9, 16
 div_biLoop:
     xor dx, dx
     movzx bx, byte[numTwo]
     div bx
-    push dx
-    mov byte[biTem2+r9], dl
+
+    mov byte[biTem3], dl
+    add byte[biTem3], '0' ;convert
+
+    push qword[biTem3]
 
     dec r9
     cmp r9, 0
     jg div_biLoop
 
     mov r9, 16
-
-    mov rax, SYS_write
-    mov rdi, STDOUT
-    mov rsi, axeIs
-    mov rdx, 5
-    syscall
 out_bi:
-    pop rbx
-    ;lea rax, [rbx + '0']
-    ;push rax
-    add rbx, '0'
-    mov byte[temp], bl
-    ;push rbx
-
     mov rax, SYS_write
     mov rdi, STDOUT
-    mov rsi, biTem2
+    mov rsi, rsp ;print bi
     mov rdx, 1
     syscall
+    pop rbx
 
     dec r9
     cmp r9, 0
-    ;pop rbx
     jg out_bi
-    ;pop rax
     
+    ;endline
+    mov rax, SYS_write
+    mov rdi, STDOUT
+    mov rsi, endline
+    mov rdx, 1
+    syscall
+
     mov rax, 60     ;exit
     mov rdi, 0
     syscall
